05230c749e0790dbff85179856cd6c3ebfca4d6f
FredrikHj <43028745+FredrikHj@users.noreply.github.com>
2023-10-11 07:47:42 +0200

    Update

--- ReqMSSQLData.ts
+++ ReqMSSQLData.ts
@@ -3,10 +3,10 @@ import {sqlConConfig} from '../../data/SQLConConfig';
 import SQLQueryBuilder from '../../data/SQLQueryBuilder';
 var sendSQLRes: Array<object> = [];
 
-export default function handler(res: any){
-    console.log("Called Server");
-   
+export default function handler(req: any, res: any, next: any){
+    
     const runSQL = async () => {      
+        console.log("Called Server");
         sql.connect(sqlConConfig).then((pool: any) => {
             return pool.request().query(SQLQueryBuilder())
         }).then((result: any) => {
@@ -15,7 +15,8 @@ export default function handler(res: any){
             res.status(200).send(sendSQLRes);
             }).catch((err: any) => {
                 res.send(err);    
-        });
+            }
+        );
     };    
    //setTimeout(() => {res.status(200).send(sendSQLRes);}, 3000);
     runSQL();
