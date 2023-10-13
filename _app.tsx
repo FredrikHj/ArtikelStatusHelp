87a7460d0c93a8c27fbcb6af8c4555f906ed5405
FredrikHj <43028745+FredrikHj@users.noreply.github.com>
2023-10-13 09:29:27 +0200

    Add files

--- _app.tsx
+++ _app.tsx
@@ -3,12 +3,14 @@ import { AppProps } from 'next/app';
 
 //Redux Store
 import { Provider } from "react-redux";
+import '../data/LoadingIndicator.css'; 
+
 import {store} from '../store';
 
 import LayoutPage from '../app/layout';
 
 //allReducers.dispatch(combineReducers);
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <LayoutPage>
